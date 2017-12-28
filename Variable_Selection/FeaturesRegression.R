3fd07f071687d7b0756432b5def4eca513e2dc06
Seonwhee-Genome <quarklep@naver.com>
2017-12-28 22:31:34 +0900

    LASSO and SIS

--- Variable_Selection/FeaturesRegression.R
+++ Variable_Selection/FeaturesRegression.R
@@ -3,19 +3,21 @@ library(data.table)
 FeatureData <- read.csv(file = "/home/seonwhee/Deep_Learning/MRImage_Pipeline/Variable_Selection/RadiomicFeatures.csv", header = TRUE, sep="\t", stringsAsFactors = FALSE)
 ## Removing general info, Feature Categories
 FeatureData <- FeatureData[-1:-9,-2:-4]
-FeatureData <- lapply(FeatureData, as.double) # change factors to doubles
-FeatureData <- as.data.frame(FeatureData)  # change list to data.frame
+FeatureData <- FeatureData %>%
+  lapply(as.double) %>% as.data.frame
+
 FeatureData <- FeatureData[complete.cases(FeatureData),]  # remove NAs
-Features <- FeatureData[,-1]
+Features <- FeatureData %>%
+  select(-(X))
 
 Response <- data.table(alive = c(0.0, 0.0, 1.0, 1.0, 0.0))   # y vector : 0 - Dead   1 - Alive
 Features <- cbind(transpose(Features), Response)
 
 ##########  Linear Regression ###############
 # Without penalty
-OLS <- lm(alive ~. , data=Features)
+OLS <- glm(alive ~. , data=Features)
 
-########## LASSO #####################
+
 library(lars)
 # library(glmnet)
 y <- Features$alive
@@ -23,15 +25,24 @@ x <- Features[,-1]
 n <- nrow(x)
 x <- matrix(unlist(x), nrow = n)  # change list to matrix
 
+########## LASSO #####################
 library(ncvreg)
 set.seed(111)
 cv_obj <- cv.ncvreg(x, y, penalty="lasso") # Cross-validation
-plot(cv_obj)
 optimal_lambda <- cv_obj$lambda.min
+plot(cv_obj)
 abline(v = log(optimal_lambda), col=4)
 
 LASSO_ncvreg <- ncvreg(x,y, penalty = "lasso", lambda = optimal_lambda) # Fit
 Estimation_ncvreg <- coef(LASSO_ncvreg)
+Estimation_ncvreg
 
-library(lsgl)
-LASSO_lsgl <- lsgl(x,y, lambda=optimal_lambda)
+#library(lsgl)
+#LASSO_lsgl <- lsgl(x,y, lambda=optimal_lambda)
+
+###### Sure Independent Screening ######
+
+library(SIS)
+SIS_obj <- SIS(x, y, family = 'binomial', penalty = 'lasso')
+SIS_obj$ix
+SIS_obj$coef.est
